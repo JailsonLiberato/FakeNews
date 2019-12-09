b72f33bc1cb82f3eb5897ea7ed1759b83e7a570d
jailsonliberato <jailsonliberato@gmail.com>
2019-12-08 23:07:24 -0300

    Carregar o vértice.

--- main.py
+++ main.py
@@ -22,15 +22,20 @@ class Main:
         """Parâmetros de pesquisa no Twitter."""
         search_words = "Bolsonaro"
         date_since = "2018-01-01"
-        date_until = "2018-11-31"
         quantity_results = 5
         self.__tweets = tweepy.Cursor(self.__api.search, q=search_words, lang="pt", since=date_since)\
             .items(quantity_results)
 
-    def execute(self):
+    def __fill_vertice(self):
+        """Carrega os vértices."""
         vertices = []
+        self.__query_tweets()
         for tweet in self.__tweets:
-            vertices.append(Vertice(tweet))
+            vertices.append(Vertice(tweet, self.__api))
+
+    def execute(self):
+        """Método principal de execução."""
+        self.__fill_vertice()
 
 
 if __name__ == "__main__":
