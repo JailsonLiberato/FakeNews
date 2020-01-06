95067361a793eb9217054720d465c0644a74beba
jailsonliberato <jailsonliberato@gmail.com>
2020-01-06 01:28:56 -0300

    Retorna o twitter com maior número de retweets.

--- main.py
+++ main.py
@@ -35,19 +35,17 @@ class Main:
         self.__network_service = NetworkService()
         self.__network = None
 
-    def execute(self):
-        """Método de execução da classe."""
-        self.__generate_tweets_json()
+    def execute(self): # self.__generate_tweets_json()
+        self.__open_json()
+        self.__build_tweet_list()
+        self.__choose_main_tweet()
+        self.__get_retweets_by_id()
+        self.__get_friends_by_id()
+        self.__get_followers_by_id()
+        self.__create_network()
+        self.__network_service.generate_network(self.__network)
+        self.__network_service.plot_network()
 
-    """" self.__open_json()
-     self.__build_tweet_list()
-     self.__choose_main_tweet()
-     self.__get_retweets_by_id()
-     self.__get_friends_by_id()
-     self.__get_followers_by_id()
-     self.__create_network()
-     self.__network_service.generate_network(self.__network)
-     self.__network_service.plot_network()"""
 
     def __create_network(self, temp_network=[], flag=False):
         """Criando a rede."""
@@ -70,12 +68,14 @@ class Main:
 
                 self.__create_network(temp_network=temp_network, flag=True)
 
+
     def __get_retweets_by_id(self):
         """Retorna uma lista de usuários que retuitaram a mensagem escolhida."""
         users_retweets = []
         for tweet in self.__twarc.retweets(self.__selected_tweet.id):
             users_retweets.append(tweet['user']['id_str'])
         self.__users_retweets = users_retweets
+
 
     def __get_friends_by_id(self):
         """Retorna a lista de friends do usuário do tweet principal."""
@@ -84,12 +84,14 @@ class Main:
             users.append(user)
         self.__friends = users
 
+
     def __get_followers_by_id(self):
         """Retorna a lista de followers do usuário do tweet principal."""
         users = []
         for user in self.__twarc.follower_ids(user=self.__selected_tweet.user_id):
             users.append(user)
         self.__followers = users
+
 
     def __build_tweet_list(self):
         """Constrói a lista de tweets."""
@@ -100,6 +102,7 @@ class Main:
                       tw['user']['friends_count']))
         self.__tweets = tweets
 
+
     def __choose_main_tweet(self):
         """Escolhe o tweet com mais followers."""
         tweet = self.__tweets[0]
@@ -108,11 +111,13 @@ class Main:
                 tweet = tw
         self.__selected_tweet = tweet
 
+
     def __check_is_retweet(self, tweettext):
         if tweettext.startswith("rt @") == True:
             print('This tweet is a retweet')
         else:
             print('This tweet is not retweet')
+
 
     def __open_json(self):
         """Abre o arquivo JSON e extrai os tweets."""
@@ -122,6 +127,7 @@ class Main:
         with open(filename_path) as json_file:
             data = json.load(json_file)
             self.__tweets = list(data['tweets'].values())
+
 
     def __generate_tweets_json(self):
         """Gerador de tweets de json."""
